4c77fb29b68d93f0757cd34d391e6e1de6a1079a
Nika <danzi6386@gmail.com>
2025-05-19 12:10:23 +0300

    proper tabs structure

--- tab.component.ts
+++ tab.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ButtonModule } from 'src/app/core/modules/button/button.module';
+import { Tab } from './tab.interface';
+
 @Component({
-	selector: 'tab',
+	imports: [CommonModule, ButtonModule],
+	selector: 'wtab',
 	templateUrl: './tab.component.html',
 	styleUrls: ['./tab.component.scss']
 })
-export class TabComponent{
-	constructor() {}
+export class TabComponent {
+	@Input() tabs: Tab[];
+
+	clear() {
+		for (const tab of this.tabs) {
+			tab.active = false;
+		}
+	}
 }
